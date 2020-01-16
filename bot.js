96a109035f0f9ea7d4140b40d6c9ec2de558edfb
D1qk <59981142+D1qk@users.noreply.github.com>
2020-01-17 01:17:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const delay = require('delay');
+const delay = require("delay");
 
-const token = "NjU5MDU3NTcxMjc3MDQ1Nzc4.XiCVOQ.pN20p4qCEQxz_fA9VDMDLdjXUPA"; // التوكن
+
 let r = 6; //عدد أعلى الرتب إلي ما راح يوصلها النشر
 const msg = `
 > **Hypixel SkyBlock Purse Shop** !
@@ -13,28 +13,38 @@ const msg = `
 let x = 0;
 let y = 0;
 
-client.on('ready', async => {
-    console.log(`Ready As ${client.user,tag}`);
+client.on('ready', async =>{
+    console.log(`Ready As ${client.user.tag}`);
     try {
-      loop(client);
-    } catch(err) {
-      console.log(`${guild.name}/Failed: ${y++}`);
+        loop(client);
+    } catch (err) {
+        console.log(`${guild.name}/Failed: ${y++}`);
     }
 });
 
 async function loop(client) {
-  for (const guild of client.guilds) {
-    await send(client, guild);
-    await delay(60000);
+
+    var guilds = await client.guilds.array();
+    for (const guild of guilds) {
+        await send(client, guild);
+        await delay(60000);
     };
+
 }
 
 async function send(client, guild) {
-  for (const member of guild.members) {
-                if (((guild.roles.array().length-1) - member.highestRole.position) <= r-1 == true) return;
-                await member.send(msg);
-                console.log(`${guild.name}/Succeeded: ${x++}`);
-                await delay(3000);
-            };
+
+    console.log(`Start Server: ${guild.name}`);
+    
+    var members = await guild.members.array();
+
+    for (const member of members) {
+        if (((guild.roles.array().length - 1) - member.highestRole.position) <= r - 1 == true) continue;
+        await member.send(msg);
+        console.log(`${guild.name}/Succeeded: ${x++}`);
+        await delay(6000);
+    };
+
 }
-client.login(token);
+
+client.login('NjU5MDU3NTcxMjc3MDQ1Nzc4.XiCVOQ.pN20p4qCEQxz_fA9VDMDLdjXUPA');
