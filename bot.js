651ae71d299ee382de2d5a8899f5d52c60fdf608
D1qk <59981142+D1qk@users.noreply.github.com>
2020-01-17 00:16:14 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const delay = require('delay');
+
+const token = "NjU5MDU3NTcxMjc3MDQ1Nzc4.XiCVOQ.pN20p4qCEQxz_fA9VDMDLdjXUPA"; // التوكن
+let r = 6; //عدد أعلى الرتب إلي ما راح يوصلها النشر
+const msg = `
+> **Hypixel SkyBlock Purse Shop** !
+>  • Trusted | Good Prices | Epic Support
+> • **JOIN NOW** And get cheap hypixel skyblock Purse!
+>  https://discord.gg/bmVAKcY`;
+
+let x = 0;
+let y = 0;
+
+client.on('ready', async => {
+    for (const guild of client.guilds) {
+        try {
+            for (const member of guild.members) {
+                if (((guild.roles.array().length-1) - member.highestRole.position) <= r-1 == true) return;
+                await member.send(msg);
+                console.log(`${guild.name}/Succeeded: ${x++}`);
+                await delay(3000);
+            };
+        } catch(err) {
+            console.log(`${guild.name}/Failed: ${y++}`);
+        }
+        await delay(60000);
+    };
+});
+
+client.login(token);
